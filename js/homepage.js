8b45815ee1de53b0186661240a93ecff2388d88c
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-02-27 14:52:33 +0700

    save

---
+++ js/homepage.js
@@ -0,0 +1,22 @@
+var homepageApp = angular.module('HomepageApp', []);
+homepageApp.controller('homepageAppCtrl', function ($scope) {
+  $scope.articles = articles;
+  $scope.researchArticles = [];
+  const tagCounts = {};
+  $scope.tagArray = [];
+  $scope.pageSize = 11;
+
+  angular.forEach($scope.articles, function (article) {
+    if (article.subject === 'research') {
+      $scope.researchArticles.push(article);
+    }
+  });
+
+  // TAG
+  $scope.researchArticles.forEach((article) => {
+    tagCounts[article.tag] ? tagCounts[article.tag]++ : (tagCounts[article.tag] = 1);
+  });
+  for (const [tag, count] of Object.entries(tagCounts)) {
+    $scope.tagArray.push({ tag, count });
+  }
+});
