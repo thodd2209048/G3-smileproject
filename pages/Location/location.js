98bca72076fadd057ef40637e792c087c3a9137d
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-03-08 11:33:38 +0700

    location, contact us

--- pages/Location/location.js
+++ pages/Location/location.js
@@ -34,6 +34,12 @@ function initMap() {
   });
 
   const image = 'logo.png';
+  const request = {
+    placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4',
+    fields: ['name', 'formatted_address', 'place_id', 'geometry'],
+  };
+  const infowindow = new google.maps.InfoWindow();
+  const service = new google.maps.places.PlacesService(map);
 
   // directionsDisplay.setMap(map);
 
